7b26465704b8453ec12e5710868d155b912e81c1
Rodrigo Souto <rnsoutob@gmail.com>
2020-04-19 18:01:09 -0300

    Enemigo2Vista wip

---
+++ classes/view/Enemigo2Vista.h
@@ -0,0 +1,14 @@
+//
+// Created by rodrigosouto on 19/4/20.
+//
+
+#ifndef CUCURUCHO_ENEMIGO2VISTA_H
+#define CUCURUCHO_ENEMIGO2VISTA_H
+
+
+class Enemigo2Vista {
+
+};
+
+
+#endif //CUCURUCHO_ENEMIGO2VISTA_H
