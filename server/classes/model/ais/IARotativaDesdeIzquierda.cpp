02a81c9915135b76b5281378bfd3de2fcf7b33dc
Rodrigo Souto <rnsoutob@gmail.com>
2020-08-04 02:37:45 -0300

    Andando patron rotativo

--- server/classes/model/ais/IARotativaDesdeIzquierda.cpp
+++ server/classes/model/ais/IARotativaDesdeIzquierda.cpp
@@ -5,7 +5,6 @@
 #include "default/IAHorizontal2.h"
 #include "../../../../commons/utils/Constantes.h"
 
-// !!!!!
 IARotativaDesdeIzquierda::IARotativaDesdeIzquierda(EntidadEnemigo *entidadEnemigo, std::map<int, Jugador *> *jugadores) {
 	this->entidadEnemigo = entidadEnemigo;
 	this->jugadores = jugadores;
@@ -13,22 +12,26 @@ IARotativaDesdeIzquierda::IARotativaDesdeIzquierda(EntidadEnemigo *entidadEnemig
 	this->ticks = 0;
 
 	float minimoMargenY = 20 + random() % 30;
-	float factodModificadorRadioY = 0.40 + random() % 15 / 100; // !!!! random
-	float margenDerecho = 50 + random() % 200; // !!!! random
-	float radio = 120 + random() % 20; // !!!! random
-	float posXDondeEmpiezaARotar = CAMPO_ANCHO - margenDerecho - entidadEnemigo->getAncho();
-	float posYDondeEmpiezaARotar = (minimoMargenY + radio * factodModificadorRadioY) +
-			random() % (long) (floor(1.0 * CAMPO_ALTO - entidadEnemigo->getAlto() - 2 * radio * factodModificadorRadioY - minimoMargenY)); // !!!! random
-	Vector posDondeEmpiezaARotar = Vector(posXDondeEmpiezaARotar, posYDondeEmpiezaARotar);
-	l->error("!!!! posDondeEmpiezaARotar " + std::to_string(posDondeEmpiezaARotar.getX()) + " - " + std::to_string(posDondeEmpiezaARotar.getY()));
+	float factodModificadorRadioY = 0.40 + random() % 15 / 100;
+	float margenDerecho = 50 + random() % 200;
+	float radio = 120 + random() % 20;
+	float posXDondeEmpiezaARotarPrimerCirculo = CAMPO_ANCHO - margenDerecho - entidadEnemigo->getAncho();
+	float posYDondeEmpiezaARotarPrimerCirculo = (minimoMargenY + radio * factodModificadorRadioY) +
+			random() % (long) (floor(1.0 * CAMPO_ALTO - entidadEnemigo->getAlto() - 2 * radio * factodModificadorRadioY - minimoMargenY));
+	Vector posDondeEmpiezaARotarPrimerCirculo = Vector(posXDondeEmpiezaARotarPrimerCirculo, posYDondeEmpiezaARotarPrimerCirculo);
 
 	// Inicializar posicion de la entidad (se podria pasar a tick idealmente).
-	entidadEnemigo->setPosicion(Vector(0, posDondeEmpiezaARotar.getY()));
+	entidadEnemigo->setPosicion(Vector(0, posDondeEmpiezaARotarPrimerCirculo.getY()));
 
-	IAEnemigo* iaCircular = new IACircular(entidadEnemigo, jugadores, 0, false, Vector(1, factodModificadorRadioY), radio);
-	IAEnemigo* iaHorizontal = new IAHorizontal2(entidadEnemigo, jugadores, true, posDondeEmpiezaARotar.getX(), iaCircular);
+	float posXDondeEmpiezaARotarSegundoCirculo = margenDerecho + 2 * radio;
 
-	this->innerIa = iaHorizontal;
+	IACircular* iaCircular2 = new IACircular(entidadEnemigo, jugadores, 0, false, Vector(1, factodModificadorRadioY), radio, 720, nullptr);
+	IAEnemigo* iaHorizontalHaciaIzquierda = new IAHorizontal2(entidadEnemigo, jugadores, false, posXDondeEmpiezaARotarSegundoCirculo, iaCircular2);
+	IAEnemigo* iaCircular1 = new IACircular(entidadEnemigo, jugadores, 0, false, Vector(1, factodModificadorRadioY), radio, 540, iaHorizontalHaciaIzquierda);
+	IAEnemigo* iaHorizontalHaciaDerecha = new IAHorizontal2(entidadEnemigo, jugadores, true, posDondeEmpiezaARotarPrimerCirculo.getX(), iaCircular1);
+	iaCircular2->setNext(iaHorizontalHaciaDerecha);
+
+	this->innerIa = iaHorizontalHaciaDerecha;
 }
 
 IAEnemigo *IARotativaDesdeIzquierda::tick() {
--- server/classes/model/ais/default/IACircular.h
+++ server/classes/model/ais/default/IACircular.h
@@ -8,19 +8,23 @@
 
 class IACircular : public IAEnemigo {
 public:
-	IACircular(EntidadEnemigo* entidadEnemigo, std::map<int, Jugador*>* jugadores, int anguloInicialGrados, bool sentidoHorario, Vector factor, float radio);
+	IACircular(EntidadEnemigo* entidadEnemigo, std::map<int, Jugador*>* jugadores, int anguloInicialGrados, bool sentidoHorario, Vector factor, float radio, int totalAnguloGrados, IAEnemigo* next);
 
 	IAEnemigo* tick() override;
+	void inicializar() override;
+	void setNext(IAEnemigo *nuevoNext);
 
 private:
 	EntidadEnemigo* entidadEnemigo;
 	std::map<int, Jugador*>* jugadores;
+	IAEnemigo* next;
 	bool primerTick;
 	int ticks;
 	Vector centro;
 	int anguloInicialGrados;
 	bool sentidoHorario;
 	float radio;
+	int totalAnguloGrados;
 	Vector factorModificadorPosicion;
 
 	Vector calcularDeltaPosicion(int anguloGrados);
